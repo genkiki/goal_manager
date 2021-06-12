17253e8d25150e985c2ff372ced41a7f23b4f3ef
genki.konda <gegege0126@gmail.com>
2021-06-12 11:42:49 +0900

    【作業中】課題編集画面の作成

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -10,4 +10,18 @@ class TasksController < ApplicationController
       redirect_to goal_path(goal_id), notice: "目標を削除しました。"
     end
   end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task.udpate!(task_params)
+    redirect_to task_path(@task.id), notice: "課題を更新しました。"
+  end
+
+  private
+  def task_params
+    params.require(:task).permit(:content, :action, :start_date, :end_date, :status)
+  end
 end

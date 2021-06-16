b870d2b62faeead57607752480714c1ffe39f619
genki.konda <gegege0126@gmail.com>
2021-06-16 16:19:36 +0900

    ログイン画面作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,8 +11,35 @@ class UsersController < ApplicationController
       session[:user_id] = @user.id
       redirect_to "/", notice: "登録成功しました"
     else
-      session[:notice] = "登録失敗しました"
+      flash[:notice] = "登録失敗しました"
       render new_user_path
+    end
+  end
+
+  def login
+  end
+
+  def signin
+    user = User.find_by(email: params[:email])
+
+    if user&.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to goals_path, notice: "ログインしました。"
+    else
+      flash[:notice] = "ログイン失敗しました"
+      render :login
+    end
+  end
+
+  def signin_easy
+    user = User.find(1)
+
+    if user&.authenticate("test")
+      session[:user_id] = user.id
+      redirect_to goals_path, notice: "ログインしました。"
+    else
+      flash[:notice] = "ログイン失敗しました"
+      render :login
     end
   end
 

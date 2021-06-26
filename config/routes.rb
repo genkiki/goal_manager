3593f71e46cebafe126a238418469025e9b472cf
genki.konda <gegege0126@gmail.com>
2021-06-26 13:44:50 +0900

    課題編集画面作成

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   post '/login', to: 'users#signin'
   get '/login_easy', to: 'users#signin_easy'
   get '/logout', to: 'users#logout'
-  resources :goals, only: [:show, :index, :edit, :destroy, :new, :create]
+  resources :goals
   resources :tasks, only: [:show, :edit, :destroy]
   resources :users do
     member do

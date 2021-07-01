be8eae753745f544c07b203fc52b6572cf75e3ec
genki.konda <gegege0126@gmail.com>
2021-07-01 10:58:59 +0900

    他ユーザー参照画面修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :authenticate_user, {only: [:show, :edit, :destroy]}
+  before_action :authenticate_user, {only: [:edit, :destroy]}
 
   def new
     @user = User.new

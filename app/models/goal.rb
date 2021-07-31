df8f37ac99f49938970e8b460e738fc2c0375a4e
genki.konda <gegege0126@gmail.com>
2021-08-01 08:54:17 +0900

    編集機能追加

--- app/models/goal.rb
+++ app/models/goal.rb
@@ -38,19 +38,11 @@ class Goal < ApplicationRecord
     end
   end
 
-  def create_notification_comment!(current_user, comment_id)
-    temp_ids = Comment.select(:user_id).where(goal_id: id).where.not(user_id: current_user.id).distinct
-    temp_ids.each do |temp_id|
-      save_notification_comment!(current_user, comment_id, temp_id['user_id'])
-    end
-    save_notification_comment!(current_user, comment_id, user_id) if temp_ids.blank?
-  end
-
-  def save_notification_comment!(current_user, comment_id, visited_id)
+  def save_notification_comment!(current_user, comment)
     notification = current_user.active_notifications.new(
       goal_id: id,
-      comment_id: comment_id,
-      visited_id: visited_id,
+      comment_id: comment.id,
+      visited_id: comment.user_id,
       action: 'comment'
     )
     if notification.visitor_id == notification.visited_id
--- config/routes.rb
+++ config/routes.rb
@@ -19,5 +19,5 @@ Rails.application.routes.draw do
   end
   resources :relationships, only: [:create, :destroy]
   resources :bookmarks, only: [:create, :destroy]
-  resources :comments, only: [:create, :edit, :destroy]
+  resources :comments, only: [:create, :destroy, :update]
 end

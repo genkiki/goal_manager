9eed56fb2af32d2a42cc21ad442b628dc6fb36c5
genki.konda <gegege0126@gmail.com>
2021-12-04 08:44:07 +0900

    user factoryの画像設定を修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,7 +3,9 @@ FactoryBot.define do
     name { "Test" }
     email { "tester@example.com" }
     password { "testtest" }
-    image { "test.jpg" }
     introduce { "test" }
+    after(:build) do |user|
+      user.image = Rack::Test::UploadedFile.new('spec/support/test_image.jpg', 'image/jpg')
+    end
   end
 end

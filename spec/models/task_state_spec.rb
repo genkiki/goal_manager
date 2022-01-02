137e8ac14afe9ad48e14f29d21dc7ebd91eab944
genki.konda <gegege0126@gmail.com>
2022-01-02 12:19:26 +0900

    TaskStateモデルのテストを追加

--- spec/models/task_state_spec.rb
+++ spec/models/task_state_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe TaskState, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "必須入力チェック" do
+    context "必須入力項目が入力されている場合" do
+      it "コメントが入力されていると有効" do
+        task_state = FactoryBot.build(:task_state)
+        expect(task_state).to be_valid
+      end
+    end
+
+    context "必須入力項目が空欄の場合" do
+      it "状態が空欄だと無効" do
+        task_state = FactoryBot.build(:task_state, state: nil)
+        expect(task_state).not_to be_valid
+      end
+    end
+  end
 end
